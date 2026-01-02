ca2588b438ad6a9d751763f8c118ce1ed6bbd2ec
Andy N <andy.nartey@googlemail.com>
2026-01-02 20:51:25 +0100

    Initialising project

---
+++ vysa/vysa/vysaApp.swift
@@ -0,0 +1,34 @@
+//
+//  vysaApp.swift
+//  vysa
+//
+//  Created by Andy on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct vysaApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ vysa/vysaTests/vysaTests.swift
@@ -0,0 +1,17 @@
+//
+//  vysaTests.swift
+//  vysaTests
+//
+//  Created by Andy on 02/01/2026.
+//
+
+import Testing
+@testable import vysa
+
+struct vysaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
